24ba2587b6d49f58a23d106facc4e34010a10637
Andrey Kolomoets <kolomoets02@gmail.com>
2023-02-27 17:35:41 +0200

    added MyLinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.SortedMap;
+
 public class Main {
     public static void main(String[] args) {
         MyArrayList<String> myArrayList = new MyArrayList<>();
@@ -18,5 +20,22 @@ public class Main {
         myArrayList.add("7");
         myArrayList.clear();
         System.out.println(myArrayList);
+        System.out.println("____________________________________");
+        MyLinkedList<String> myLinkedList = new MyLinkedList<>();
+        myLinkedList.add("1");
+        myLinkedList.add("2");
+        myLinkedList.add("3");
+        System.out.println(myLinkedList);
+        myLinkedList.size();
+        myLinkedList.add("4");
+        myLinkedList.add("5");
+        myLinkedList.add("6");
+        myLinkedList.add("7");
+        myLinkedList.remove(0);
+        myLinkedList.size();
+        System.out.println(myLinkedList);
+        myLinkedList.clear();
+        System.out.println(myLinkedList);
+        myLinkedList.size();
     }
 }
